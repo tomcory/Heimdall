c3bd4c96eccf57ff38a458de1d80340b59f826de
Thomas Cory <cory@tu-berlin.de>
2023-12-23 15:16:26 +0100

    implemented PermissionScannerViewModel

--- app/src/main/java/de/tomcory/heimdall/ui/scanner/permission/PermissionScannerViewModel.kt
+++ app/src/main/java/de/tomcory/heimdall/ui/scanner/permission/PermissionScannerViewModel.kt
@@ -1,13 +1,29 @@
 package de.tomcory.heimdall.ui.scanner.permission
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
+import dagger.hilt.android.qualifiers.ApplicationContext
+import de.tomcory.heimdall.MonitoringScopeApps
+import de.tomcory.heimdall.core.database.entity.App
+import de.tomcory.heimdall.core.scanner.PermissionScanner
+import de.tomcory.heimdall.ui.scanner.ScannerRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
 
-class PermissionScannerViewModel : ViewModel() {
+class PermissionScannerViewModel @Inject constructor(
+    @SuppressLint("StaticFieldLeak") @ApplicationContext private val context: Context,
+    private val repository: ScannerRepository
+) : ViewModel() {
 
     val scanActiveInitial = false
     val scanProgressInitial = 0f
@@ -32,18 +48,13 @@ class PermissionScannerViewModel : ViewModel() {
 
     fun onScan(onShowSnackbar: (String) -> Unit) {
         viewModelScope.launch {
-            var scanCancelled = false
             _scanActive.emit(true)
-            _scanProgress.emit(0f)
-            for (i in 0..100) {
-                if (!scanActive.value) {
-                    scanCancelled = true
-                    break
-                }
-                _scanProgress.emit(scanProgress.value + 0.01f)
-                delay(30)
-            }
-            if (scanCancelled) {
+
+            val scanResult = scanAllApps(context)
+
+            if (scanResult) {
+                onShowSnackbar("Scan completed.")
+            } else {
                 onShowSnackbar("Scan cancelled.")
             }
             _scanActive.emit(false)
@@ -60,11 +71,109 @@ class PermissionScannerViewModel : ViewModel() {
         TODO("Not yet implemented")
     }
 
-    fun onShowHelp() {
-        TODO("Not yet implemented")
-    }
-
     ///////////////////////////////
     // Private methods
     ///////////////////////////////
+
+    private suspend fun scanAllApps(context: Context) : Boolean {
+
+        _scanProgress.emit(0f)
+
+        // fetch necessary preferences
+        val scope = repository.preferences.permissionMonitoringScope.first()
+        val whitelist = repository.preferences.permissionWhitelist.first()
+        val blacklist = repository.preferences.permissionBlacklist.first()
+
+        // create a permission scanner
+        val permissionScanner = PermissionScanner()
+
+        _scanProgress.emit(0.03f)
+
+        // get a list of all installed apps
+        val pm: PackageManager = context.packageManager
+        val packages = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pm.getInstalledPackages(PackageManager.PackageInfoFlags.of(PackageManager.GET_META_DATA.toLong()))
+        } else {
+            pm.getInstalledPackages(PackageManager.GET_META_DATA)
+        }
+
+        _scanProgress.emit(0.08f)
+
+        // filter the list of apps according to the scope
+        val filtered = packages.filter(getScanPredicate(scope, whitelist, blacklist))
+        Timber.d("Scanning ${filtered.size} apps in monitoring scope $scope...")
+
+        _scanProgress.emit(0.1f)
+
+        // calculate the progress that each individual app scan contributes
+        val progressStep = 0.89f / filtered.size
+        var progressValue = 0.1f
+
+        // scan each app
+        filtered.forEach {
+
+            // check if the scan has been cancelled
+            if (!scanActive.value) {
+                return false
+            }
+
+            // persist the app in the database
+            repository.persistApp(
+                App(
+                    packageName = it.packageName,
+                    label = it.applicationInfo.loadLabel(pm).toString(),
+                    versionName = it.versionName ?: "",
+                    versionCode = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        it.longVersionCode
+                    } else {
+                        it.versionCode.toLong()
+                    },
+                    isSystem = it.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM != 0
+                )
+            )
+
+            // scan the permissions of the app (also persists them)
+            try {
+                permissionScanner.scanApp(it)
+            } catch (e: Exception) {
+                Timber.e(e, "Error scanning permissions of ${it.packageName}")
+            }
+
+            // update the progress
+            progressValue += progressStep
+            _scanProgress.emit(progressValue)
+        }
+
+        _scanProgress.emit(1f)
+        return true
+    }
+
+    private fun getScanPredicate(
+        scope: MonitoringScopeApps,
+        whitelist: List<String>,
+        blacklist: List<String>
+    ): (PackageInfo) -> Boolean = when(scope) {
+
+        MonitoringScopeApps.APPS_ALL, MonitoringScopeApps.UNRECOGNIZED -> {
+            _ -> true
+        }
+
+        MonitoringScopeApps.APPS_NON_SYSTEM -> {
+            packageInfo -> packageInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0
+        }
+
+        MonitoringScopeApps.APPS_WHITELIST -> {
+            packageInfo -> whitelist.contains(packageInfo.packageName)
+        }
+
+        MonitoringScopeApps.APPS_BLACKLIST -> {
+            packageInfo -> !blacklist.contains(packageInfo.packageName)
+
+        }
+
+        MonitoringScopeApps.APPS_NON_SYSTEM_BLACKLIST -> {
+            packageInfo -> packageInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0
+                && !blacklist.contains(packageInfo.packageName)
+        }
+    }
 }
--- core/scanner/src/main/java/de/tomcory/heimdall/core/scanner/PermissionScanner.kt
+++ core/scanner/src/main/java/de/tomcory/heimdall/core/scanner/PermissionScanner.kt
@@ -7,9 +7,9 @@ import de.tomcory.heimdall.core.database.entity.Permission
 import timber.log.Timber
 import javax.inject.Inject
 
-class PermissionScanner @Inject constructor(
-    private val database: HeimdallDatabase
-) {
+class PermissionScanner {
+
+    @Inject lateinit var database: HeimdallDatabase
 
     /**
      * List of all dangerous permissions as of Android 13
