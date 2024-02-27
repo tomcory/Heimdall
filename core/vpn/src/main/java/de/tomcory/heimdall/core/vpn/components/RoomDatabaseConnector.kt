c41c50d72a88c1f81d3e447db1a9b19c261e8891
Tom Cory <cory@tu-berlin.de>
2024-02-27 20:55:10 +0100

    added exception handling to RoomDatabaseConnector

--- core/vpn/src/main/java/de/tomcory/heimdall/core/vpn/components/RoomDatabaseConnector.kt
+++ core/vpn/src/main/java/de/tomcory/heimdall/core/vpn/components/RoomDatabaseConnector.kt
@@ -5,17 +5,28 @@ import de.tomcory.heimdall.core.database.entity.Connection
 import de.tomcory.heimdall.core.database.entity.Request
 import de.tomcory.heimdall.core.database.entity.Response
 import de.tomcory.heimdall.core.database.entity.Session
+import timber.log.Timber
 
 class RoomDatabaseConnector(
     val database: HeimdallDatabase
 ): DatabaseConnector {
     override suspend fun persistSession(startTime: Long): Int {
-        val ids = database.sessionDao().insert(Session(startTime = startTime))
+        val ids = try {
+            database.sessionDao().insert(Session(startTime = startTime))
+        } catch (e: Exception) {
+            Timber.e(e, "Error while persisting session")
+            emptyList()
+        }
         return if (ids.isNotEmpty()) ids.first().toInt() else -1
     }
 
     override suspend fun updateSession(id: Int, endTime: Long): Int {
-        return database.sessionDao().updateEndTime(id, endTime)
+        return try {
+            database.sessionDao().updateEndTime(id, endTime)
+        } catch (e: Exception) {
+            Timber.e(e, "Error while updating session")
+            -1
+        }
     }
 
     override suspend fun persistTransportLayerConnection(
@@ -31,21 +42,26 @@ class RoomDatabaseConnector(
         remotePort: Int,
         isTracker: Boolean
     ): Int {
-        val ids = database.connectionDao().insert(
-            Connection(
-                sessionId = sessionId,
-                protocol = protocol,
-                ipVersion = ipVersion,
-                initialTimestamp = initialTimestamp,
-                initiatorId = initiatorId,
-                initiatorPkg = initiatorPkg,
-                localPort = localPort,
-                remoteHost = remoteHost,
-                remoteIp = remoteIp,
-                remotePort = remotePort,
-                isTracker = isTracker
+        val ids = try {
+            database.connectionDao().insert(
+                Connection(
+                    sessionId = sessionId,
+                    protocol = protocol,
+                    ipVersion = ipVersion,
+                    initialTimestamp = initialTimestamp,
+                    initiatorId = initiatorId,
+                    initiatorPkg = initiatorPkg,
+                    localPort = localPort,
+                    remoteHost = remoteHost,
+                    remoteIp = remoteIp,
+                    remotePort = remotePort,
+                    isTracker = isTracker
+                )
             )
-        )
+        } catch (e: Exception) {
+            Timber.e(e, "Error while persisting transport layer connection (sID: $sessionId)")
+            emptyList()
+        }
         return if (ids.isNotEmpty()) ids.first().toInt() else -1
     }
 
@@ -65,24 +81,29 @@ class RoomDatabaseConnector(
         initiatorId: Int,
         initiatorPkg: String
     ): Int {
-        val ids = database.requestDao().insert(
-            Request(
-                connectionId = connectionId,
-                timestamp = timestamp,
-                headers = if(headers.isNotEmpty()) headers.map { "${it.key}: ${it.value}" }.reduce { acc, s -> "$acc$s\n" } else "",
-                content = content,
-                contentLength = contentLength,
-                method = method,
-                remoteHost = remoteHost,
-                remotePath = remotePath,
-                remoteIp = remoteIp,
-                remotePort = remotePort,
-                localIp = localIp,
-                localPort = localPort,
-                initiatorId = initiatorId,
-                initiatorPkg = initiatorPkg
+        val ids = try {
+            database.requestDao().insert(
+                Request(
+                    connectionId = connectionId,
+                    timestamp = timestamp,
+                    headers = if(headers.isNotEmpty()) headers.map { "${it.key}: ${it.value}" }.reduce { acc, s -> "$acc$s\n" } else "",
+                    content = content,
+                    contentLength = contentLength,
+                    method = method,
+                    remoteHost = remoteHost,
+                    remotePath = remotePath,
+                    remoteIp = remoteIp,
+                    remotePort = remotePort,
+                    localIp = localIp,
+                    localPort = localPort,
+                    initiatorId = initiatorId,
+                    initiatorPkg = initiatorPkg
+                )
             )
-        )
+        } catch (e: Exception) {
+            Timber.e(e, "Error while persisting http request (cID: $connectionId)")
+            emptyList()
+        }
         return if (ids.isNotEmpty()) ids.first().toInt() else -1
     }
 
@@ -103,25 +124,30 @@ class RoomDatabaseConnector(
         initiatorId: Int,
         initiatorPkg: String
     ): Int {
-        val ids = database.responseDao().insert(
-            Response(
-                connectionId = connectionId,
-                requestId = requestId,
-                timestamp = timestamp,
-                headers = if(headers.isNotEmpty()) headers.map { "${it.key}: ${it.value}" }.reduce { acc, s -> "$acc$s\n" } else "",
-                content = content,
-                contentLength = contentLength,
-                statusCode = statusCode,
-                statusMsg = statusMsg,
-                remoteHost = remoteHost,
-                remoteIp = remoteIp,
-                remotePort = remotePort,
-                localIp = localIp,
-                localPort = localPort,
-                initiatorId = initiatorId,
-                initiatorPkg = initiatorPkg
+        val ids = try {
+            database.responseDao().insert(
+                Response(
+                    connectionId = connectionId,
+                    requestId = requestId,
+                    timestamp = timestamp,
+                    headers = if(headers.isNotEmpty()) headers.map { "${it.key}: ${it.value}" }.reduce { acc, s -> "$acc$s\n" } else "",
+                    content = content,
+                    contentLength = contentLength,
+                    statusCode = statusCode,
+                    statusMsg = statusMsg,
+                    remoteHost = remoteHost,
+                    remoteIp = remoteIp,
+                    remotePort = remotePort,
+                    localIp = localIp,
+                    localPort = localPort,
+                    initiatorId = initiatorId,
+                    initiatorPkg = initiatorPkg
+                )
             )
-        )
+        } catch (e: Exception) {
+            Timber.e(e, "Error while persisting http response (cID: $connectionId, rID: $requestId)")
+            emptyList()
+        }
         return if (ids.isNotEmpty()) ids.first().toInt() else -1
     }
 }
